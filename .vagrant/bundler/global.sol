d6c14c5bf0f06a1bda7c1d037226970e0301790d
Joao Almeida <jalmeida@MacBook-Pro-de-Joao.local>
2021-08-20 11:11:33 +0100

    change cluster cidr - 192.168.100.0/24

--- .vagrant/bundler/global.sol
+++
@@ -1,1 +0,0 @@
-{"dependencies":[["vagrant-hosts",["= 2.9.0"]],["log4r",[">= 0"]],["concurrent-ruby",["~> 1.0"]],["i18n",[">= 0"]],["micromachine",[">= 2","< 4"]],["vagrant-vbguest",["= 0.30.0"]],["vagrant-vboxmanage",["= 0.0.2"]]],"checksum":"1ab07dff8d62c6e56a520b08dcf820442d08eddb20b5ebd01d8fc7ae14fd8f20","vagrant_version":"2.2.18"}
